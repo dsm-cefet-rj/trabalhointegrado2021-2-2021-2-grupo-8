0c6f7f61af424c7c6b56769e727ff11263ae92ee
Nickolas Barbosa <81327961+nicko-dev@users.noreply.github.com>
2021-12-20 19:12:07 -0300

    componentizacao 2

--- project-simple/src/components/Home/Home.jsx
+++ project-simple/src/components/Home/Home.jsx
@@ -1,107 +1,64 @@
-import React, { Fragment, useEffect, useState } from "react";
-import { Link, Route, Routes, useParams } from "react-router-dom";
-import CardTarefa from "./CardTarefa";
-import teamsSheet from "../../data/dataTeams.json";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import MenuHome from "./MenuHome";
 import tasksSheet from "../../data/dataTasks.json";
 import eventsSheet from "../../data/dataEvents.json";
+import QuadroTarefas from "./QuadroTarefas";
+import Tarefa from "../Tarefa/Tarefa";
 
-function Home({ login }) {
-  const { idTeam } = useParams();
-
-  const [equipe, setEquipe] = useState();
+function Home({ login, equipe }) {
+  const [equipeAtiva, setEquipeAtiva] = useState({});
   const [tarefas, setTarefas] = useState({
     paradas: [],
     andamento: [],
     minhas: [],
   });
-  const [eventos, setEventos] = useState();
 
   useEffect(() => {
-    const eq = teamsSheet.filter((e) => {
-      return idTeam == e.id;
-    });
-    setEquipe(eq[0]);
-  }, [idTeam]);
+    setEquipeAtiva(equipe);
 
-  useEffect(() => {
-    if (typeof equipe != "undefined") {
-      setTarefas(() => {
-        const tarefasAtivas = tasksSheet.filter((t) => {
-          return equipe.id === t.idTeam;
-        });
+    setTarefas(() => {
 
-        const tarefasParadas = tarefasAtivas.filter((t) => {
-          return t.idResponsavel === 0 && equipe.id === t.idTeam;
-        });
-
-        const tarefasAndamento = tarefasAtivas.filter((t) => {
-          return t.idResponsavel !== 0 && equipe.id === t.idTeam;
-        });
-
-        const minhasTarefas = tarefasAtivas.filter((t) => {
-          return t.idResponsavel === login.id && equipe.id === t.idTeam;
-        });
-
-        return {
-          paradas: tarefasParadas,
-          andamento: tarefasAndamento,
-          minhas: minhasTarefas,
-        };
+      const tarefasParadas = equipe.tarefas.filter((t) => {
+        return t.idResponsavel == 0;
       });
-      setEventos(
-        eventsSheet.filter((e) => {
-          return equipe.id === e.idTeam;
-        })
-      );
-    }
+
+      const tarefasAndamento = equipe.tarefas.filter((t) => {
+        return t.idResponsavel != 0;
+      });
+
+      const minhasTarefas = equipe.tarefas.filter((t) => {
+        return t.idResponsavel == login.id;
+      });
+
+      return {
+        paradas: tarefasParadas,
+        andamento: tarefasAndamento,
+        minhas: minhasTarefas,
+      };
+    });
   }, [equipe]);
 
-  console.log(equipe);
-  console.log(tarefas);
-  console.log(eventos);
-
-  const renderTarefas = (t) => {
-    return <CardTarefa tarefa={t} />;
+  const atribuirTarefa = (tarefa) => {
+    console.log("Atribui");
+    console.log(tarefa);
   };
 
-  if (typeof equipe != "undefined") {
+  if (typeof equipeAtiva != "undefined") {
     return (
       <div className="corpo">
         <header className="container cabecalho">
           <h1 className="app-name">Project Simple</h1>
         </header>
         <main className="container">
-          <h3 className="text-center my-4">{"Equipe " + equipe.name}</h3>
-          <section>
-            <div className="nome-tabela">
-              <h2>Tarefas paradas</h2>
-            </div>
-            <div className="tabela-tarefas d-flex flex-wrap justify-content-evenly">
-              {tarefas.paradas.map(renderTarefas)}
-            </div>
-          </section>
-          <section className="mt-4">
-            <div className="nome-tabela">
-              <h2>Tarefas em Andamento</h2>
-            </div>
-            <div className="tabela-tarefas d-flex flex-wrap justify-content-evenly">
-              {tarefas.andamento.map(renderTarefas)}
-            </div>
-          </section>
-          <section className="my-4">
-            <div className="nome-tabela">
-              <h2>Minhas Tarefas</h2>
-              {tarefas.minhas.map(renderTarefas)}
-            </div>
-            <div className="tabela-tarefas d-flex flex-wrap justify-content-evenly"></div>
-          </section>
-          <section className="menu">
-            <span className="btn btn-primary">Gerenciar Eventos</span>
-            <span className="btn btn-primary">Gerenciar Equipe</span>
-            <Link to={"/"}>
-              <span className="btn btn-primary">Minhas Equipes</span>
-            </Link>
-          </section>
+          <h3 className="text-center my-4">{"Equipe " + equipe.info.name}</h3>
+          <QuadroTarefas
+            tarefas={tarefas}
+            gerente={equipeAtiva.isGerente}
+            atribuirTarefa={atribuirTarefa}
+          />
+
+          <MenuHome gerente={equipeAtiva.isGerente} equipe={equipeAtiva} />
         </main>
       </div>
     );
