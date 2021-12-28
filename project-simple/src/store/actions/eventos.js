413ed08e25b26e28eb616cb908d8954d99c9a974
Nickolas Barbosa <81327961+nicko-dev@users.noreply.github.com>
2021-12-28 17:52:38 -0300

    redux 1

--- project-simple/src/store/actions/eventos.js
+++
@@ -1,9 +0,0 @@
-import eventsSheet from '../../data/dbEvents.json'
-
-export default function addEventosReducer(eventos, novoEvento){
-    const lastId = eventsSheet.slice(-1)[0].idEvent;
-    // novoEvento.idTeam = equipeAtiva.info.id;
-    novoEvento.idEvent = lastId + 1;
-    
-    return eventos;
-}
